1378b86b5c62c9ecb8a6d8fcb854f585a529a6d1
Lachlan Jowett <lachlan@jowett.net.au>
2023-07-07 19:29:23 +1000

    fixed some issues with auth and working on frontend

--- server/server.js
+++ server/server.js
@@ -6,10 +6,11 @@ const jwt = require('jsonwebtoken')
 const mongo = require('mongodb').MongoClient
 const bcrypt = require('bcrypt')
 var cookieParser = require('cookie-parser')
+var https = require('node:https')
 app.use(express.json())
 app.use(cookieParser())
 
-const url = 'mongodb://127.0.0.1:27017'
+const url = 'mongodb://127.0.0.1:27017/'
 const dbName = 'Volunteer'
 const serveRoot = "C:\\Users\\lochy\\OneDrive\\Documents\\GitHub\\Volunteer"
 
@@ -17,14 +18,31 @@ async function startServer() {
     console.log("Starting server...");
 
     // Open Mongo Connection
-    const client = await mongo.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-
-    const db = await client.db(dbName)
-    const users = await db.collection('Users')
+    var {client, db, users} = await connectDB();
 
     // Start Express Server
 
-    // Public facing pages
+    registerPublicPages();
+
+    registerAuthPages(users);
+
+    registerAppPages(users);
+
+    console.log("HTTP server started on port 80");
+    console.log("http://localhost:80/");
+    app.listen(80);
+}
+
+async function connectDB() {
+    const client = await mongo.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    const db = client.db(dbName);
+    const users = db.collection('Users');
+
+    return {client, db, users};
+}
+
+async function registerPublicPages() {
     app.get('/', (req, res) => {
         res.sendFile('./pages/index.html', { root: serveRoot })
     });
@@ -37,17 +55,12 @@ async function startServer() {
         res.sendFile('./pages/login.html', { root: serveRoot })
     });
 
-    registerAuthPages(users);
-
-    registerAppPages(users);
-
-    console.log("Server started on port 80");
-    console.log("http://localhost:80");
-    app.listen(80);
+    // register static files
+    app.use(express.static('resources'))
 }
 
 async function registerAuthPages(DBCollection) {
-    await app.post('/auth/register', async(req, res) => {
+    app.post('/auth/register', async(req, res) => {
 
         if (!req.body.username || !req.body.password) {
             res.status(400).json({
@@ -63,21 +76,24 @@ async function registerAuthPages(DBCollection) {
     
         const isUsernameTaken = await userExists(username, DBCollection);
 
-        if (isUsernameTaken) {
+        if (isUsernameTaken == true) {
             res.status(400).json({ error: 'Username already exists' })
+
             return;
-        }
-    
-        if (!isUsernameTaken) {
+
+        } else if (isUsernameTaken == false) {
             const hash = await bcrypt.hash(password, 10)
-    
+
             addUserToDB(username, hash, email, phone_number, user_type, DBCollection)
 
             res.status(200).json({ message: 'User created' })
+
+        } else {
+            res.status(500).json({ error: 'Something went wrong' })
         }
     })
-    
-    await app.post('/auth/token', (req, res) => {
+
+    app.post('/auth/token', (req, res) => {
     
         const refreshToken = req.body.token
 
@@ -97,12 +113,12 @@ async function registerAuthPages(DBCollection) {
     })
     
     // Delete the refresh token from the array of refresh tokens
-    await app.delete('/auth/logout', (req, res) => {
+    app.delete('/auth/logout', (req, res) => {
         refreshTokens = refreshTokens.filter(token => token !== req.body.token)
         res.sendStatus(204)
     })
     
-    await app.post('/auth/login', async(req, res) => {
+    app.post('/auth/login', async(req, res) => {
         const {username, password} = req.body;
 
         if (!username || !password)
@@ -110,7 +126,7 @@ async function registerAuthPages(DBCollection) {
     
         const doesUsernameExist = await userExists(username, DBCollection);
 
-        if (doesUsernameExist) {
+        if (doesUsernameExist == true) {
 
             const userCredentials = await getUserFromDB(username, DBCollection)
             const storedHashedPassword = userCredentials[0].password
@@ -138,7 +154,7 @@ async function registerAuthPages(DBCollection) {
 
 }
 
-function registerAppPages(DBCollection) {
+async function registerAppPages(DBCollection) {
 
     app.get('/app/', authenticateToken, (req, res) => {
         res.sendFile('./app/homepage.html', { root: serveRoot })
@@ -152,28 +168,65 @@ function registerAppPages(DBCollection) {
         res.sendFile('./app/settings.html', { root: serveRoot })
     });
 
-    app.get('/app/user/:username/', authenticateToken, (req, res) => {
+    app.get('/app/user/:username/', authenticateToken, async (req, res) => {
         const username = req.params.username;
 
         if (!username) {
             res.redirect('/app/');
         }
 
+        console.log(username)
 
+        SSRUserPage(username, getUser(req.cookies.accessToken), DBCollection);
+
+        const data = await getUserFromDB(username, DBCollection);
+
+        res.send(data);
 
     });
+}
+
+async function SSRUserPage(username, accessor, DBCollection) {
+
+    if (userExists(username, DBCollection) == false)
+        return;
+
+    const permissionLevel = await userPermissions(accessor, DBCollection);
+
+    //const userPage = await getUserFromDB(username, DBCollection);
+
+    switch (permissionLevel) {
+        case 0:
+        case 1:
+            console.log("User is a volunteer");
+        case 2:
+            console.log("User is a mod");
+        case 3:
+            console.log("User is an admin");
+    }
+
+
+}
+
+function getUser(token) {
+    let User;
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return null;
+        User = user;
+    })
+    return User.name;
 }
 
 // Permission ranges from 0 to 2
 // 0 is a regular user accessing another users profile
 // 1 is a moderator accessing a users profile
 // 2 is an admin accessing a users profile or a user accessing their own profile
-function userPermissions(username, accessingUsername, collection) {
+async function userPermissions(username, accessingUsername, collection) {
     if (username == accessingUsername) {
         return 2;
     }
 
-    getUserFromDB(username, collection).then((result) => {
+    await getUserFromDB(username, collection).then((result) => {
         if (result[0].user_type == 3) // 3 is admin, 2 is moderator, 1 is organization, 0 is volunteer
             return 2;
         else if (result[0].user_type == 2)
@@ -185,20 +238,20 @@ function userPermissions(username, accessingUsername, collection) {
     return false;
 }
 
-function userExists(username, collection) {
-    return collection.find({ username: username }).toArray((err, result) => {
+function filterUserdata(userdata, permissionLevel) {
 
-        if (err) {
-            mongoError(err, "userExists - collection.find");
-        }
+}
 
+async function userExists(username, collection) {
+    const arr = await collection.find({ username: username }).toArray();
 
-        if (result.length > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    })
+    if (arr.length > 0) {
+        return true;
+    } else if (arr.length == 0) {
+        return false;
+    } else {
+        return null;
+    }
 
 }
 
@@ -216,19 +269,20 @@ function addUserToDB(username, password, email, phoneNumber, userType, collectio
 }
 
 function getUserFromDB(username, collection) {
-    return collection.find({ username: username }).toArray((err, result) => {
+    return collection.find({ username: username }).toArray();
+    // return collection.find({ username: username }).toArray((err, result) => {
 
-        if (err) {
-            mongoError(err, "getUserFromDB - collection.find");
-        }
+    //     if (err) {
+    //         mongoError(err, "getUserFromDB - collection.find");
+    //     }
 
-        if (result.length > 1) {
-            mongoError(err, "getUserFromDB - collection.find returned more than one result for users");
-        } else {
-            return result;
-        }
+    //     if (result.length > 1) {
+    //         mongoError(err, "getUserFromDB - collection.find returned more than one result for users");
+    //     } else {
+    //         return result;
+    //     }
 
-    })
+    // })
 }
 
 function generateAccessToken(user) {
